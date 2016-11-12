5d6b7d63cebf3e09f500d84ac3cd21c0f62a202d
Peerchemist <dx.1178@gmail.com>
2016-11-12 15:52:24 +0100

    update debian and pip files.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='peercoin_rpc',
-      version='0.41',
+      version='0.43',
       description='Library to communicate with peercoin daemon via JSON-RPC protocol.',
       url='https://github.com/peerchemist/peercoin_rpc',
       author='Peerchemist',
