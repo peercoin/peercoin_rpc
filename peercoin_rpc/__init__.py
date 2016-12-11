4a9d76d36ac7f9368dc1513b71e73f1a2ac4fde1
Peerchemist <dx.1178@gmail.com>
2016-12-11 11:27:28 +0100

    version bump.

--- peercoin_rpc/__init__.py
+++ peercoin_rpc/__init__.py
@@ -4,4 +4,4 @@ try:
 except ImportError:
     from peercoin_rpc import Client
 
-__version__ = 0.45
+__version__ = 0.46
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='peercoin_rpc',
-      version='0.45',
+      version='0.46',
       description='Library to communicate with peercoin daemon via JSON-RPC protocol.',
       url='https://github.com/peerchemist/peercoin_rpc',
       author='Peerchemist',
