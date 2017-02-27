7e502f8c4025e46194b83daa05478838ab1d60e8
Peerchemist <dx.1178@gmail.com>
2017-02-27 11:32:38 +0100

    version bump to 0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='peercoin_rpc',
-      version='0.49',
+      version='0.50',
       description='Library to communicate with peercoin daemon via JSON-RPC protocol.',
       url='https://github.com/peerchemist/peercoin_rpc',
       author='Peerchemist',
