abce6b272e2038af283b04ff5efe389c0d43abbb
Peerchemist <dx.1178@gmail.com>
2018-08-07 10:07:42 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup
+from os import path
+
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Financial and Insurance Industry',
+  'Programming Language :: Python',
+  'Operating System :: OS Independent',
+  'Natural Language :: English',
+  'License :: OSI Approved :: MIT License'
+]
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='peercoin_rpc',
-      version='0.57',
+      version='0.58',
       description='Library to communicate with peercoin daemon via JSON-RPC protocol.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/peercoin/peercoin_rpc',
       author='Peerchemist',
       author_email='peerchemist@protonmail.ch',
@@ -10,13 +27,5 @@ setup(name='peercoin_rpc',
       packages=['peercoin_rpc'],
       install_requires=['requests'],
       keywords=['peercoin', 'json-rpc', 'cryptocurrency', 'blockchain'],
-      classifiers=[
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6'
-      ],
+      classifiers=classifiers,
       platforms="any")
